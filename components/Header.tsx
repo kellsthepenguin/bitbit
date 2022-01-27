973dfdfca3aae041a47fd68e0ffbcf3d8e517bae
Kim Jioo <jwkim101201@gmail.com>
2022-01-27 14:25:29 +0900

    remove: remove unnecessary element, classes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,16 @@
 const Header = () => {
   return (
     <nav
-      className='flex items-center justify-between flex-wrap bg-teal p-6'
+      className='flex items-center justify-between flex-wrap p-6'
     >
-      <div className='flex flex-no-shrink text-white mr-6'>
-        <span className='font-semibold text-3xl tracking-tight text-blue-600'>Bitbit</span>
+      <div className='flex mr-6'>
+        <span className='font-semibold text-3xl text-blue-600'>Bitbit</span>
       </div>
-      <div className='w-full block flex-grow flex w-auto'>
-        <div className='text-sm flex-grow'>
-        </div>
+      <div className='w-full block flex w-auto'>
         <div>
           <a
             href='#'
-            className='inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:text-gray-500 mt-0'
+            className='inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:text-gray-500'
           >
             Login
           </a>
