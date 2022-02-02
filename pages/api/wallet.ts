ff8fdfb3543a434d00bbcb1556901015d595580a
Kim Jioo <jwkim101201@gmail.com>
2022-02-03 08:53:12 +0900

    feat: wallet

---
+++ pages/api/wallet.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { JwtPayload } from 'jsonwebtoken'
+
+import { jwtMiddleware } from '../../utils'
+import { prisma } from '../../global'
+
+interface Wallet {
+  ownerId?: string
+  krw: bigint
+  usd: number
+  coins: any
+  idx?: number
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  jwtMiddleware(req, res, (payload: JwtPayload) => {
+    if (req.method === 'GET') return get(req, res, payload)
+    res.json({ error: 'bad req' })
+  })
+}
+
+async function get(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+  payload: JwtPayload
+) {
+  const wallet = await prisma.wallet.findFirst({
+    where: {
+      ownerId: payload.id
+    }
+  }) as Wallet
+
+  delete wallet.idx
+  delete wallet.ownerId
+
+  res.json(wallet)
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,29 @@
 import { randomBytes } from 'crypto'
+import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken'
+import { key } from '../global'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 function generateRandomString() {
   return randomBytes(128).toString('utf-8').replaceAll('\u0000', '')
 }
 
+function jwtMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  cb: (payload: JwtPayload) => any
+) {
+  const { token } = req.query
+
+  if (!token) return res.json({ error: 'token is not provided' })
+  jwt.verify(token as string, key, (err: VerifyErrors | null, token?: JwtPayload | string) => {
+    if (err) return res.json({ error: 'an error occured during checking token' })
+
+    cb(token as JwtPayload)
+  })
+}
+
 export {
-  generateRandomString
+  generateRandomString,
+  jwtMiddleware
 }
